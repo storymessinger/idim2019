aff1daf3119499bd7502bf356c37fb71691724f9
Jundong <storymessinger@gmail.com>
2019-12-05 22:25:36 +0900

    project add

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -37,8 +37,14 @@
       Industrial design of a healthcare product for a startup based on patented method of detecting micro-organisms using projection through water.
     </h3>
 
-    <div id="img-1">
-      <img src="../assets/18_WAVETALK/1.png" class="project-img">
+    <div id="imgbox-1" class="project-img">
+      <div class="content">
+        <img src="../assets/18_WAVETALK/1.png" >
+      </div>
+      <p>
+        <span>Fig 22-02</span>
+        Fabric Module Usage
+      </p>
     </div>
 
     <div class="project-paragraph">
@@ -58,22 +64,43 @@
 
     </div>
 
-    <div id="img-2">
-      <img src="../assets/18_WAVETALK/10.png" class="project-img">
+    <div id="imgbox-2" class="project-img">
+      <div class="content">
+        <img src="../assets/18_WAVETALK/10.png">
+      </div>
+      <p>
+        <span>Fig 22-02</span>
+        Fabric Module Usage
+      </p>
     </div>
+
+    <div id="imgbox-3-4">
+      <div id="imgbox-3" class="project-img">
+        <div class="content">
+          <img src="../assets/18_WAVETALK/UI-01.gif" >
+        </div>
+        <p>
+          <span>Fig 22-03</span>
+        </p>
+      </div>
+      <div id="imgbox-4" class="project-img">
+        <div class="content">
+          <img src="../assets/18_WAVETALK/7b.png" >
+        </div>
+        <p>
+          <span>Fig 22-04</span>
+        </p>
+      </div>
+    </div>
+
 
     <div class="project-paragraph">
       <div>
-        <h3>Redefining the Identity</h3>
+        <h3>Visual Identity</h3>
       </div>
       <div class="project-paragraph-body">
         <p>
-          Based in Daejeon (South Korea) and Seoul (South Korea), Wavetalk is a Biology stary up with the ambitious mission of decoding the world of bacteria for the benefit of people who are prone to bacterial disease, such as infants or elders.
-          <br>
-          ID+IM design laboratory developed the design of Wavetalk’s first commercial product line for everyday use. The project was led by ID+IM’s creative director Sangmin Bae.
-        </p>
-        <p>
-          The design language of bacteria is typically anonymous, unable to be detected through bare eyes. Eschewing this, the design team decided to focus on a design that represents connection, and the potential to expand the functionality. They did this by first re-designing the Wavetalk visual identity and bringing the meaning into life.
+          ID+IM Design Laboratory interpreted the identity of Wavetalk as if the wave itself is talking to the opposite medium and receiving an answer back. Design team depicted this idea into three letter alphabet ‘TWT’ which stands for ‘The Wave Talks’, but also images a wave going to and back between different materials.
         </p>
       </div>
 
@@ -129,22 +156,70 @@ h3 {
   margin-bottom: 80px;
 }
 
-#img-1 {
-  display: flex;
-  justify-content: center; /* align horizontal */
-  margin-bottom: 80px;
-}
-
-#img-2 {
-  display: flex;
-  justify-content: flex-start; /* align horizontal */
-  margin-left: 76px;
-  margin-right:254px;
-  margin-bottom: 160px;
+.project-img {
   img {
-    width: 100%;
+    width: 100%
+  }
+  p {
+    span {
+      margin-right: 1rem;
+    }
+    margin-top: 16px;
+    font-size: 16px;
+    font-weight: 600;
   }
 }
+
+#imgbox-1 {
+  width: 100%;
+
+  display: flex;
+  flex-flow:column;
+  align-items: center;
+
+  margin-bottom: 80px;
+
+  .content, p {
+    width: 1000px;
+  }
+}
+
+#imgbox-2 {
+  width: 100%;
+
+  display: flex;
+  justify-content: flex-start; /* align horizontal */
+  flex-flow:column;
+
+  margin-bottom: 40px;
+
+  .content, p {
+    margin-left: 76px;
+    margin-right:254px;
+  }
+
+}
+
+#imgbox-3-4 {
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 76px;
+  width: 100%;
+
+  #imgbox-3 {
+    margin-bottom: 160px;
+    .content, p {
+      width: 650px;
+      margin-right: 200px;
+    }
+  }
+  #imgbox-4 {
+    .content, p {
+      width: 300px;
+    }
+  }
+}
+
 
 .project-paragraph {
   display: flex;
