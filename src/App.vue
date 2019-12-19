347fd01d8b7c64743465fb714a4a219a5b95580e
Kwang Eun Park <paukparliament@gmail.com>
2019-12-19 12:49:31 +0900

    routing implemented

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,95 @@
 
     <div class="back-layer">
 
-      <div class="logo-box">
+      <div class="logo-box"
+        @click="pushRoute()">
         <!-- to be replaced by img? -->
         <h1>ID+IM</h1> 
       </div>
 
       <nav class="menu">
         <ul>
-          <li>Work</li>
-          <li>About</li>
+          <li 
+            :class="`
+              ${$route.params.dir1 === 'work' ? 'on' : ''}
+            `"
+            @click="pushRoute('work', null, null, 'all')"
+          >
+            <h2>Work</h2>
+          </li>
+          <li :class="`
+            ${$route.params.dir1 === 'about' ? 'on' : ''}
+          `"
+            @click="pushRoute('about')"
+          >
+            <h2>About</h2>
+          </li>
         </ul>
         <ul class="bottom">
-          <li>Contact</li>
-          <li>Shop</li>
+          <li>
+            <h2>Contact</h2>
+          </li>
+          <li>
+            <h2>Shop</h2>
+          </li>
         </ul>
       </nav>
 
-      <nav class="sub-menu">
-        <ul>
-          <li v-for="filter in workFilterList" :key="filter"> 
-              {{filter}}
-            </li>
-        </ul>
-      </nav>
-
-       <nav class="work-list">
+      <transition name="appear">
+        <nav class="sub-menu"
+          v-if="$route.params.dir1 === 'work'">
           <ul>
-            <li v-for="(work, index) in workList" :key="work"> 
-              <span>{{formatIndex(workList.length-index)}}</span>{{work}}
+            <li v-for="filter in workFilterList" :key="filter"
+              @click="pushRoute('work', null, null, filter)"
+              :class="`${$route.hash === '#'+filter ? 'on' : ''}`" 
+            > 
+              <h2>{{filter.replace(/\b\w/, filter.charAt(0).toUpperCase())}}</h2>
             </li>
-
           </ul>
         </nav>
-
+      </transition>
+      <transition name="appear">
+        <nav class="work-list"
+          v-if="$route.params.dir1 === 'work'">
+          <ul>
+            <li v-for="(work, index) in workList" :key="work"
+              @click="pushRoute('work', index.toString())"
+              :class="`${$route.params.dir2 == index ? 'on' : ''}`" 
+            >
+              <h2>
+                <span>{{formatIndex(workList.length-index)}}</span>{{work}}
+              </h2>
+            </li>
+          </ul>
+        </nav>
+      </transition>
+    </div>
 
     
+    <transition name="slide">
+      <div class="front-layer"
+      v-if="$route.params.dir2"
+      >
+        <div 
+          :class="`
+            click-layer 
+            ${$route.params.dir2 ? 'clickable' : ''}
+          `"
+          v-if="$route.params.dir2"
+          @click="pushRoute('work', null, null, 'all')"
+        >
+        </div>
+        <div
+          class="work-paper"
+          
+        >
 
-    </div>
-
-    <div class="front-layer">
-
-    </div>
+        </div>
+      </div>
+    </transition>
+    
   </div>
 </template>
-
 
 <script>
 
@@ -55,36 +100,84 @@ export default {
   data: function(){
     return {
       workFilterList: [
-        'All',
-        'Nanum',
-        'Research',
-        'Client',
-        'Exhibition'
+        'all',
+        'nanum',
+        'research',
+        'client',
+        'exhibition'
       ],
       workList: [
         'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library',
-        'KAIST Library'
+        'Humicotta',
+        'S.Cone',
+        'KAIST-Audi Creative Lounge',
+        'Maasai Smart Cane',
+        'Light Funnel',
+        'Culture BOXCHOOL',
+        'H-Chromosome',
+        'BOXCHOOL',
+        'Alive Tent',
+        'Social Wind',
+        'T2B',
+        'tent3.0',
+        'Stream Cooler',
+        'Roll-Di',
+        'Printing Solar Cell',
+        'Snowenergy',
+        'Sound Spray',
+        "D'light",
+        'Namsun',
+        'Grass Paint',
+        'Baby Lamp',
+        'Lovepot',
+        'Heartea',
+        'Crosscube',
+        'Roly-Poly',
+        'Clothtag',
+        'Bottomup',
+        'Pingpong Screen',
+        'RhythmFish',
+        '35mm Camera',
+        'Sound Pump'
       ]
     }
   },
+  mounted: function() {
+    console.log(this.$route);
+  },
+  updated: function() {
+    console.log(this.$route);
+  },
   methods : {
     formatIndex : index => index < 10 ? '0'+index.toString() : index,
+    pushRoute: function(dir1, dir2, dir3, hash) {
+      this.$router.push(
+        {
+          path: 
+            '/' +
+            (dir1 ? dir1 : '') +
+            (dir2 ? '/' + dir2 : '') +
+            (dir3 ? '/' + dir3 : ''),
+          hash
+        },  
+        null, 
+        ()=>{/* override abort callback */}
+      );
+    },
+    replaceRoute: function(dir1, dir2, dir3, hash) {
+      this.$router.replace(
+        {
+          path: 
+            '/' +
+            (dir1 ? dir1 : '') +
+            (dir2 ? '/' + dir2 : '') +
+            (dir3 ? '/' + dir3 : ''),
+          hash
+        },  
+        null, 
+        ()=>{/* override abort callback */}
+      );
+    },
   },
   components: {
     
@@ -110,7 +203,7 @@ export default {
 
 <style lang="scss">
 @import './normalize.scss';
-@import './overrideNormalize.scss';
+
 
 // Fonts
 @font-face {
@@ -139,7 +232,45 @@ export default {
   --left-1: 30px;
   --left-2: 180px;
   --left-3: 400px;
-  --indent: 30px;
+  --indent: 20px;
+  --list-height: 35px;
+}
+
+body {
+  position: fixed;
+  top: 0; left: 0;
+  width: 100%; height: 100%;
+  background: blue;
+  overflow: hidden;
+}
+html {
+  line-height: 1;
+}
+
+h1 {
+  margin: 0;
+  font-size: auto;
+  font-weight: 400;
+}
+
+h2 {
+  margin: 0;
+  font-size: auto;
+  font-weight: 400;
+}
+
+p {
+  margin: 0;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+div {
+  box-sizing: border-box;
 }
 
 
@@ -148,19 +279,29 @@ export default {
   font-family: Suisse, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: black;
+  position: absolute;
+  top: 0; left: 0;
+  width: 100%; height: 100%;
+  overflow: scroll;
+
+  
+  
   
   .back-layer {
     position: fixed;
     top: 0; left: 0;
     width: 100%; height: 100%;
-    background: #33f;
+    background: #f82;
+    color:#a30;
     font-size: 30px;
+    overflow-y: scroll;
     
     .logo-box {
-      position: absolute;
+      position: fixed;
       top: var(--top-1);
       left: var(--left-1);
+      cursor: pointer;
+
       h1 {
         font-size: 1em;
       }
@@ -171,24 +312,42 @@ export default {
       display: flex;
       flex-direction: column;
       align-items: flex-start;
-      overflow: hidden;
+      overflow: visible;
+      width: 0;
       li {
         height: 1.15em;
+        height: var(--list-height);
         position: relative;
         padding-left: var(--indent);
-        transition: transform 0.15s;
+        transition: transform 0.1s;
         transform: translateX(calc(-1 * var(--indent)));
         overflow: hidden;
         cursor: pointer;
 
         &:hover {
           transform: translateX(0);
+          color: #03f;
+        }
+
+        h2 {
+          font-size: 30px;
+          line-height: var(--list-height);
+          white-space: nowrap;
+          
+          &::selection, span::selection{
+            background: #0f0;
+          }
+        }
+
+        &.on {
+          transform: translateX(0);
+          color: #03f;
         }
       }
     }
 
     .menu ul {
-      position: absolute;
+      position: fixed;
       top: var(--top-2);
       left: var(--left-1);
 
@@ -196,33 +355,80 @@ export default {
         top: auto;
         bottom: var(--top-1);
       }
-
-      li {
-      }
     }
 
     .sub-menu ul {
-      position: absolute;
+      position: fixed;
       top: var(--top-2);
       left: var(--left-2);
+      /* z-index: -1; */
     }
 
-    .work-list ul {
-      position: absolute;
-      top: var(--top-2);
-      left: var(--left-3);
+    .work-list {
+      ul {
+        /* position: absolute; */
+        position: relative;
+        margin: var(--top-2) 0 var(--top-2) var(--left-3);
+        z-index:100;
 
-      span {
-        font-family: SuisseMono;
-        margin: 0 0.8em 0 0;
+        h2 {
+          span {
+            font-size: 28px;
+            font-family: SuisseMono;
+            margin: 0 0.8em 0 0;
+          }
+        }
       }
     }
+
+    .appear-enter-active, .appear-leave-active {
+      transition: opacity 0.4s, transform 0.4s;
+    }
+    .appear-enter, .appear-leave-to /* .fade-leave-active below version 2.1.8 */ {
+      opacity: 0;
+      transform: translate(-20px, 0);
+    }
   }
 
+  
+
+
   .front-layer {
-    /* position: fixed;
-    top: 0; */
+    position: relative;
+    width: 100vw;
+    padding: 50vh 0 50vh 10%;
+    /* will-change: transform; */
+
+    .click-layer {
+      position: absolute;
+      top: 0; left: 0; bottom: 0; right: 0;
+
+      &:hover {
+        cursor: e-resize;
+      }
+    }
+
+    .work-paper {
+      position: relative;
+      width: 100%; height: 2000px;
+      z-index: 11;
+      background: white;
+      box-shadow: 0 10px 20px 5px rgba(0,0,0,0.2);
+      border-radius: 2px;
+    }
+    
   }
+  .slide-enter-active, .slide-leave-active {
+      transition: transform 0.5s;
+      transition-timing-function: cubic-bezier(0.165, 0.840, 0.440, 1.000);
+    }
+    .slide-enter, .slide-leave-to {
+      transform: translate(100vw, 0);
+    }
+ 
+
+
+  
 
 
 }
--- src/main.js
+++ src/main.js
@@ -19,20 +19,20 @@ Vue.config.productionTip = false;
 const routes = [
   {
     path: '/',
-    name: 'home',
-    // component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/:dir1',
+  },
+  {
+    path: '/:dir1/:dir2',
+  },
+  {
+    path: '/:dir1/:dir2/:dir3',
+  },
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
